9ced3974093fe028e09f6bd29cfc36bf7a7b451e
moahmed amine mezghich <git@mezghich.com>
2022-10-12 15:51:24 +0100

    exemple de props

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View, StyleSheet,Image } from 'react-native';
 
-export default function App() {
+const styles = StyleSheet.create({
+  center: {
+    alignItems: 'center'
+  },
+img: {
+  width: '80%',
+  height:'40%',
+  marginBottom:'20%',
+  marginTop:'20%',
+}
+
+})
+
+const Greeting = (props) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.center}>
+      <Text>Hello {props.firstname}!  {props.lastname}!</Text>
+      <Image style={styles.img}
+       source={{uri: 'https://www.plb.fr/app/images/logo-plb/officiel/logo-plb-Avenir.svg'}} 
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const LotsOfGreetings = () => {
+  return (
+    <View style={[styles.center, {top: 50}]}>
+      <Greeting firstname='Amine' lastname='Mezghich' />
+      <Greeting firstname='Kamel' lastname='Mezghich' />
+      <Greeting firstname='Bertrand' lastname='Mezghich' />
+     
+    </View>
+  );
+}
+
+export default LotsOfGreetings;
