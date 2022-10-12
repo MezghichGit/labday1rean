f5b473e1df0289901455a90773a776bc3b9db8eb
moahmed amine mezghich <git@mezghich.com>
2022-10-12 16:13:36 +0100

    exemple avec hook useState

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Text, View, StyleSheet,Image } from 'react-native';
+import React, {useState} from 'react';
+import { Text, View, StyleSheet,Image,Button } from 'react-native';
 
 const styles = StyleSheet.create({
   center: {
@@ -26,12 +26,29 @@ const Greeting = (props) => {
 }
 
 const LotsOfGreetings = () => {
+ 
+  const [count, setCompteur] = useState(0);
+  const [greetNbr, setGetNbr] = useState(10);
+
+  function increment(){
+    setCompteur(count+1);
+    setGetNbr(greetNbr+1);
+  }
+
+  function decrement(){
+    setCompteur(count-1);
+    setGetNbr(greetNbr-1);
+  }
+
   return (
     <View style={[styles.center, {top: 50}]}>
       <Greeting firstname='Amine' lastname='Mezghich' />
       <Greeting firstname='Kamel' lastname='Mezghich' />
       <Greeting firstname='Bertrand' lastname='Mezghich' />
-     
+      <Text>{count}</Text>
+      <Text>Nbre de Greet initial : {greetNbr}</Text>
+      <Button title="incrémenter" onPress={increment}></Button>
+      <Button title="décrémenter" onPress={decrement}></Button>
     </View>
   );
 }
